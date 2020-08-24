61874119cb65f47d0b5e3a293c5b9f7ccb8e6c97
Eisten <eisten.flores@unmsm.edu.pe>
2020-08-24 10:34:31 -0500

    Añadiendo método handler ver

--- src/main/java/com/bolasaideas/springboot/error/app/controllers/AppController.java
+++ src/main/java/com/bolasaideas/springboot/error/app/controllers/AppController.java
@@ -1,16 +1,32 @@
 package com.bolasaideas.springboot.error.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.bolasaideas.springboot.error.app.models.domain.Usuario;
+import com.bolasaideas.springboot.error.app.services.UsuarioService;
 
 @Controller
 public class AppController {
 
+	@Autowired
+	private UsuarioService usuarioService;
+
 	@GetMapping("/index")
 	public String index() {
 		/* Integer valor = 100 / 0; */
-
 		Integer valor = Integer.parseInt("10x");
 		return "index";
+	}
+
+	@GetMapping("/ver/{id}")
+	public String ver(@PathVariable Integer id, Model model) {
+		Usuario usuario = usuarioService.ObtenerPorId(id);
+		model.addAttribute("titulo", "Detalle Usuario :".concat(usuario.getNombre()));
+		model.addAttribute("usuario", usuario);
+		return "ver";
 	}
 }
